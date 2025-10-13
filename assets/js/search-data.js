6f7303c2d2fde86c19110b8cce861e716387a66a
vytrucle-02 <trucvylepro@gmail.com>
2025-10-13 19:24:24 +0000

    Deploying to gh-pages from @ vytrucle-02/academic@8c4e5f1d18881d1828ba774b257fec6566bccf14 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,19 +23,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/academic/blog/";
           },
+        },{id: "nav-service",
+          title: "service",
+          description: "Materials for courses you taught. Replace this text with your description.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/academic/service/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/academic/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/academic/teaching/";
           },
         },{id: "post-a-post-with-plotly-js",
         
