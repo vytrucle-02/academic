a5e228705899193ea48d9e7fbb84734dde7b0a8e
vytrucle-02 <trucvylepro@gmail.com>
2025-10-13 21:35:15 +0000

    Deploying to gh-pages from @ vytrucle-02/academic@99b4f79190689ac3edd19cc0dda9dd691fe9a283 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -459,35 +459,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%76%79%74%72%75%63%6C%65.%6A%75%64%79@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/vy-truc-le-a10155276", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("/academic/feed.xml", "_blank");
+          window.open("https://orcid.org/0009-0002-1008-2380", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=pnxR0u0AAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-researchgate',
+        title: 'ResearchGate',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.researchgate.net/profile/https://www.researchgate.net/profile/Vy-Le-121?ev=hdr_xprf/", "_blank");
         },
       },{
       id: 'light-theme',
