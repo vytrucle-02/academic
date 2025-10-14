b25f6f02cae11db9fca67ef6a6bd43089486542b
vytrucle-02 <trucvylepro@gmail.com>
2025-10-14 03:12:04 +0000

    Deploying to gh-pages from @ vytrucle-02/academic@fba9f341640e48720b9bb2021717c48f0c785ffc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "You can view my academic profile here or click the PDF icon for full details.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/academic/cv/";
